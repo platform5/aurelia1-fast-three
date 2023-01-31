95c1c77cc6b8b79f1f4c7725e1d08e13560b200d
Tamu <5020311+Tamu@users.noreply.github.com>
2023-01-31 10:19:57 +0100

    Add FAST

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { inject, BindingEngine, bindable } from 'aurelia-framework';
+
+
 export class App {
   public message = 'Hello World!';
+
+  constructor(private element: Element, private bindingEngine: BindingEngine) {
+  }
+
+  private clic() : void {
+
+    console.log('clic');
+
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import {Aurelia} from 'aurelia-framework';
 import environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
+import {provideFASTDesignSystem, fastCard, fastButton, fastTextField} from '@microsoft/fast-components';
+
+provideFASTDesignSystem()
+    .register(
+        fastCard(),
+        fastButton(),
+        fastTextField()
+    );
 
 export function configure(aurelia: Aurelia): void {
   aurelia.use
     .standardConfiguration()
     .feature(PLATFORM.moduleName('resources/index'));
 
+    aurelia.use
+    .plugin(PLATFORM.moduleName('aurelia1-fast-adapter'))
+    
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
   if (environment.testing) {
